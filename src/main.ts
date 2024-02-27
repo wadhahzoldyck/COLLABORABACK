bcd33d4ae2a793a9b42ec41a0ad4f1f98eb280f5
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-02-27 23:30:00 +0100

    crud content

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as multer from 'multer';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(multer({ dest: './uploads' }).single('file')); // Change dest to your desired upload directory
+
+
   await app.listen(3000);
 }
 bootstrap();
