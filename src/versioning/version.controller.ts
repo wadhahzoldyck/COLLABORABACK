f4cf888fa5ec142c58b143940c6abb6580c4a92e
Oussemamri <oussema9100@gmail.com>
2024-02-27 23:42:08 +0100

    modif crud

--- src/versioning/version.controller.ts
+++ src/versioning/version.controller.ts
@@ -1,45 +1,23 @@
+// version.controller.ts
 
-import { Body, Controller, Delete, Get, Param, Put, Version } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Put } from '@nestjs/common';
+import { documents } from './mock-data';
 
 @Controller({ path: 'hello' })
 export class VersionController {
     @Get('document/:version')
     findAll(@Param('version') version: string) {
-      switch (version) {
-        case 'v1':
-          return 'This is version 1 of the document route.';
-        case 'v2':
-          return 'This is version 2 of the document route.';
-        case 'v3':
-            return 'This is version 3 of the document route.';
-        default:
-          return 'Invalid version.';
-      }
+        const document = documents.find(doc => doc.version === version);
+        return document ? document.content : 'Document not found.';
     }
+
     @Delete('document/:version')
     delete(@Param('version') version: string) {
-      switch (version) {
-        case 'v1':
-          return 'Delete operation for version 1 of the document route.';
-        case 'v2':
-          return 'Delete operation for version 2 of the document route.';
-          case 'v3':
-            return 'Delete operation for version 3 of the document route.';
-        default:
-          return 'Invalid version.';
-      }
+        return `Delete operation for version ${version} of the document route.`;
     }
+
     @Put('document/:version')
     update(@Param('version') version: string, @Body() body: any) {
-      switch (version) {
-        case 'v1':
-          return `Update operation for version 1 of the document route. Data: ${JSON.stringify(body)}`;
-        case 'v2':
-          return `Update operation for version 2 of the document route. Data: ${JSON.stringify(body)}`;
-        case 'v3':
-            return `Update operation for version 3 of the document route. Data: ${JSON.stringify(body)}`;
-        default:
-          return 'Invalid version.';
-      }
+        return `Update operation for version ${version} of the document route. Data: ${JSON.stringify(body)}`;
     }
 }
