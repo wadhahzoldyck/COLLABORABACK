279c997b90e644bec387de64d27c98513c2c003c
Oussemamri <oussema9100@gmail.com>
2024-03-13 15:35:36 +0100

    test

--- src/versioning/mock-data.ts
+++ src/versioning/mock-data.ts
@@ -1,7 +1,7 @@
 // mock-data.ts
 
 export const documents = [
-    { id: 1, version: 'v1', content: 'Document content for version 1' },
+    { id: 1, version: 'v1', content: 'Regular exercise is crucial for maintaining physical and mental health, promoting longevity and well-being. Prioritizing regular exercise is essential for a healthy lifestyle' },
     { id: 2, version: 'v2', content: 'Document content for version 2' },
     { id: 3, version: 'v3', content: 'Document content for version 3' },
     // Add more documents as needed
