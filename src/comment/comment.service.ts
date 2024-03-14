f95585578320c407883fc448ad1b3a738474f49a
Malak-hadj-messaoud <hadjmessaoud.malak@esprit.tn>
2024-03-14 15:28:53 +0100

    socket comment

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,41 +1,53 @@
+// comment.service.ts
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Comment } from './schema/comment.schema';
+import { Comment, CommentDocument } from './schema/comment.schema';
+import { Reply } from 'src/reply/schema/reply.schema';
 
 @Injectable()
 export class CommentService {
-    constructor(
-        @InjectModel(Comment.name) private readonly commentModel: Model<Comment>,
-      ) {}
-    
-    async create(CommentData: any): Promise<Comment> {
-        const createdComment = new this.commentModel(CommentData);
-        return createdComment.save();
-      }
-    async update(commentId: string, updatedData: any): Promise<Comment | null> {
-        const updatedComment = await this.commentModel.findByIdAndUpdate(
-            commentId,
-            updatedData,
-            { new: true } 
-        );
-        return updatedComment;
-    }
-    async delete(commentId: string): Promise<void> {
-        const deletedComment = await this.commentModel.findByIdAndDelete(commentId);
+  constructor(
+    @InjectModel(Comment.name) private readonly commentModel: Model<CommentDocument>,
+  ) {}
 
-        if (!deletedComment) {
-            throw new NotFoundException(`Comment with ID ${commentId} not found`);
-        }
-    }
+  async create(commentData: any): Promise<Comment> {
+    const createdComment = new this.commentModel(commentData);
+    return createdComment.save();
+  }
 
-    async findById(commentId: string): Promise<Comment | null> {
-        const comment = await this.commentModel.findById(commentId).exec();
-        return comment;
+  async update(commentId: any, updatedData: any): Promise<Comment | null> {
+    const updatedComment = await this.commentModel.findByIdAndUpdate(
+      commentId,
+      updatedData,
+      { new: true },
+    );
+    return updatedComment;
+  }
+  async findRepliesByCommentId(commentId: string): Promise<Reply[]> {
+    const comment = await this.commentModel.findById(commentId).populate('replies').exec();
+    if (!comment) {
+      throw new NotFoundException(`Comment with ID ${commentId} not found`);
     }
-    async findAll(): Promise<Comment[]> {
-        const comments = await this.commentModel.find().exec();
-        return comments;
+    return comment.replies;
+  }
+  
+  async delete(commentId: string): Promise<void> {
+    const deletedComment = await this.commentModel.findByIdAndDelete(commentId);
+
+    if (!deletedComment) {
+      throw new NotFoundException(`Comment with ID ${commentId} not found`);
     }
+  }
+
+  async findById(commentId: string): Promise<Comment | null> {
+    const comment = await this.commentModel.findById(commentId).exec();
+    return comment;
+  }
+
+  async findAll(): Promise<Comment[]> {
+    const comments = await this.commentModel.find().exec();
+    return comments;
+  }
 
 }
