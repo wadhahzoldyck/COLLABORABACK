e1f230c259f8671d3a1db0d842b260a53c2ce185
wadhah naggui <wadhah.naggui@esprit.tn>
2024-03-28 15:51:24 +0000

    fixing document name

--- src/document/schema/document.schema.ts
+++ src/document/schema/document.schema.ts
@@ -1,34 +1,34 @@
-//document.schema.ts
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+  //document.schema.ts
+  import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
-import {  Schema as MongooseSchema } from 'mongoose';
+  import {  Schema as MongooseSchema } from 'mongoose';
 
-import { User } from '../../auth/schema/user.schema'; // Import the User schema
+  import { User } from '../../auth/schema/user.schema'; // Import the User schema
 
 
-import { Comment } from '../../comment/schema/comment.schema';
+  import { Comment } from '../../comment/schema/comment.schema';
 
-@Schema({
-  timestamps: true,
-})
-export class Document   {
+  @Schema({
+    timestamps: true,
+  })
+  export class Document   {
+      @Prop()
+    _id: string;
     @Prop()
-  _id: string;
-  @Prop()
-  documentName: string;
+    documentName: string;
 
-  @Prop({ type: Object })
-  data: Object;
+    @Prop({ type: Object })
+    data: Object;
 
-  @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'User' }) 
-  owner: User;
+    @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'User' }) 
+    owner: User;
 
-  @Prop({ type: [{ type: MongooseSchema.Types.ObjectId, ref: 'User' }] }) 
-  usersWithAccess: User[];
+    @Prop({ type: [{ type: MongooseSchema.Types.ObjectId, ref: 'User' }] }) 
+    usersWithAccess: User[];
 
-   @Prop({ type: [{ type: MongooseSchema.Types.ObjectId, ref: 'Comment' }] })
-  comments: Comment[];
+    @Prop({ type: [{ type: MongooseSchema.Types.ObjectId, ref: 'Comment' }] })
+    comments: Comment[];
 
-}
+  }
 
-export const DocumentSchema = SchemaFactory.createForClass(Document);
+  export const DocumentSchema = SchemaFactory.createForClass(Document);
