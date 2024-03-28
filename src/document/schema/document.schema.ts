43521969d1196b6777cc0160a682b475dfdcefa4
wadhah naggui <wadhah.naggui@esprit.tn>
2024-03-28 13:19:31 +0000

    documentSchema

--- src/document/schema/document.schema.ts
+++ src/document/schema/document.schema.ts
@@ -1,14 +1,23 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import {  Schema as MongooseSchema } from 'mongoose';
+
+import { User } from '../../auth/schema/user.schema'; // Import the User schema
 
 @Schema({
   timestamps: true,
 })
-export class Document {
+export class Document   {
   @Prop()
-  _id: string
+  documentName: string;
+
   @Prop({ type: Object })
   data: Object;
 
+  @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'User' }) 
+  owner: User;
 
+  @Prop({ type: [{ type: MongooseSchema.Types.ObjectId, ref: 'User' }] }) 
+  usersWithAccess: User[];
 }
+
 export const DocumentSchema = SchemaFactory.createForClass(Document);
