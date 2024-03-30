ed53778fcd5cd7cab45f335360f13fe425b94f65
Oussemamri <oussema9100@gmail.com>
2024-03-30 14:02:18 +0100

    adding versioning schema and module

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import * as cookiesPaser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: '*',
     credentials: true,
   });
     app.use(multer({ dest: './uploads' }).single('file')); // Change dest to your desired upload directory
