f87db4bc424ccd79cb846b3fc9a973a9b96e21c9
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-04-05 00:01:16 +0100

    unit test

--- src/document/document.controller.spec.ts
+++ src/document/document.controller.spec.ts
@@ -1,18 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { DocumentController } from './document.controller';
+import { DocumentService } from './document.service';
 
-describe('DocumentController', () => {
-  let controller: DocumentController;
+describe('DocumentService', () => {
+  let service: DocumentService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [DocumentController],
+      providers: [DocumentService],
     }).compile();
 
-    controller = module.get<DocumentController>(DocumentController);
+    service = module.get<DocumentService>(DocumentService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
