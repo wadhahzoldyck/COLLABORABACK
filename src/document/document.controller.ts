5896c7469606cc178194e2f224d165d9811dda1c
wadhah naggui <wadhah.naggui@esprit.tn>
2024-04-04 23:03:41 +0000

    push

--- src/document/document.controller.ts
+++ src/document/document.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+// document.controller.ts
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
+import { DocumentService } from './document.service';
+import { Document } from './schema/document.schema';
+import { AddUserToDocumentDto } from './dto/document.dto';
 
 @Controller('document')
-export class DocumentController {}
+export class DocumentController {
+  constructor(private readonly documentService: DocumentService) {}
+
+
+
+  @Get('doc/:id')
+  async getDocumentById(@Param('id') id: string): Promise<Document> {
+    return this.documentService.getDocumentById(id);
+  }
+
+  @Get('owner/:id')
+  async getByOwnerId(@Param('id') ownerId: string): Promise<Document[]> {
+    const documents = await this.documentService.findByOwnerId(ownerId);
+
+  
+
+    return documents;
+  }
+
+  @Post('add-user')
+  async addUserToDocument(@Body() dto: AddUserToDocumentDto) {
+    return await this.documentService.addUserToDocument(dto);
+  }
+
+  @Get(':id/access-users')
+  async getUsersWithAccess(@Param('id') id: string): Promise<string[]> {
+    return this.documentService.getUsersWithAccess(id);
+  }
+}
