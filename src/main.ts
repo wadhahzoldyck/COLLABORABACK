487385595c42b5f52b5146dcb14f08bfb02095d6
Oussemamri <oussema9100@gmail.com>
2024-04-04 21:30:34 +0100

    adding functions

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import * as cookiesPaser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173','http://localhost:3001','http://localhost:3002'],
+    origin: '*',
     credentials: true,
   });
     app.use(multer({ dest: './uploads' }).single('file')); // Change dest to your desired upload directory
