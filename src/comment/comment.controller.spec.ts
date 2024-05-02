dbde3f62121ab7feac86c8e07209264aadcbf095
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-05-02 19:11:14 +0100

    Final commit

--- src/comment/comment.controller.spec.ts
+++ src/comment/comment.controller.spec.ts
@@ -1,7 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
-import { CommentSchema } from './schema/comment.schema'; // Import the CommentModel
+import { CommentSchema } from '../comment/schema/comment.schema'; // Import CommentModel from your application
+import { UserSchema } from '../auth/schema/user.schema'; // Import UserModel from your application
+import { DocumentSchema } from '../document/schema/document.schema'; // Import DocumentModel from your application
 
 describe('CommentController', () => {
   let controller: CommentController;
@@ -11,12 +13,16 @@ describe('CommentController', () => {
       controllers: [CommentController],
       providers: [
         CommentService,
+        // Provide CommentModel, UserModel, and DocumentModel as part of the testing module
         { provide: 'CommentModel', useValue: CommentSchema },
+        { provide: 'UserModel', useValue: UserSchema },
+        { provide: 'DocumentModel', useValue: DocumentSchema },
       ],
     }).compile();
 
     controller = module.get<CommentController>(CommentController);
   });
+
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
