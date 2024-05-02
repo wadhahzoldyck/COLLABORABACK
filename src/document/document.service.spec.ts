8b3aa5878c3e92cd8f034f9ec19b6f02fb336db5
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-05-02 19:23:30 +0100

    Final commit

--- src/document/document.service.spec.ts
+++ src/document/document.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DocumentService } from './document.service';
 import { DocumentSchema } from './schema/document.schema';
+import { UserSchema } from '../auth/schema/user.schema';
 
 describe('DocumentService', () => {
   let service: DocumentService;
@@ -10,12 +11,12 @@ describe('DocumentService', () => {
       providers: [
         DocumentService,
         { provide: 'DocumentModel', useValue: DocumentSchema },
+        { provide: 'UserModel', useValue: UserSchema }, 
       ],
     }).compile();
 
     service = module.get<DocumentService>(DocumentService);
   });
-
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
