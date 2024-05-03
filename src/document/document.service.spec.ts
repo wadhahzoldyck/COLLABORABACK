49286843cf013668aa1aee21adfef48a6bc40470
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-05-03 08:29:16 +0100

    Final commit

--- src/document/document.service.spec.ts
+++ src/document/document.service.spec.ts
@@ -3,6 +3,7 @@ import { DocumentService } from './document.service';
 import { DocumentSchema } from './schema/document.schema';
 import { UserSchema } from '../auth/schema/user.schema';
 import { FolderSchema } from '../folder/schema/folder.schema';
+import {  WorkspaceSchema } from '../workspace/schema/workspace.schema';
 
 describe('DocumentService', () => {
   let service: DocumentService;
@@ -14,6 +15,8 @@ describe('DocumentService', () => {
         { provide: 'DocumentModel', useValue: DocumentSchema },
         { provide: 'UserModel', useValue: UserSchema },
         { provide: 'FolderModel', useValue: FolderSchema }, // Provide FolderSchema
+        { provide: 'WorkspaceModel', useValue: WorkspaceSchema }, // Provide FolderSchema
+
       ],
     }).compile();
 
